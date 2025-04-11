01b851a7ecff6fd7202bdc6a39f9e353dcf3b2d2
ssomasundaram <ssomasundaram@netskope.com>
2025-04-11 08:05:58 +0530

    BWAN-9819: CLI to configure tcp timeout per neighbor
    new CLI to configure TCP timeout per neighbor
    CLI will be used in FRR configure automation in gw/controller
    
    (cherry picked from commit c6507adb9d57709be079059778615916dd172837)

--- bgpd/bgp_fsm.c
+++ bgpd/bgp_fsm.c
@@ -1303,6 +1303,20 @@ static int bgp_connect_success(struct peer *peer)
 		return -1;
 	}
 
+	int val = atomic_load(&peer->tcpusrto);
+
+        if (CHECK_FLAG(peer->flags, PEER_FLAG_TCP_USER_TIMEOUT) &&
+            atomic_load(&peer->tcpusrto) > 0) {
+                if (setsockopt(peer->fd, IPPROTO_TCP, TCP_USER_TIMEOUT,
+                               &val, sizeof(val)) < 0) {
+                        zlog_warn("Failed to set TCP_USER_TIMEOUT for peer %s: %s",
+                                  peer->host, safe_strerror(errno));
+                } else {
+                        zlog_debug("TCP_USER_TIMEOUT set for peer %s to %d ms",
+                                   peer->host, val);
+                }
+        }
+
 	bgp_reads_on(peer);
 
 	if (bgp_debug_neighbor_events(peer)) {
